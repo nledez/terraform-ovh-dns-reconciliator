b5759a26576e6a9ca14cca86c1462db369dff853
Nicolas Ledez <github.public@ledez.net>
2020-03-20 21:35:22 +0100

    First test with load a tfstate with one record

--- tests/test_terraform_ovh_dns_reconciliator.py
+++ tests/test_terraform_ovh_dns_reconciliator.py
@@ -1,5 +1,24 @@
 import pytest
+import os
+
+from terraform_ovh_dns_reconciliator import load_tfstate
+
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
-def test_is_false():
-    assert False is True
+def test_load_tfstate_with_one_dns_record():
+    ex1_filepath = os.path.join(DIR_PATH, 'terraform_ex1.tfstate')
+    tfstate_content = load_tfstate(ex1_filepath)
+
+    tfstate_assert = [
+        {
+            "fieldtype": "A",
+            "id": "5110317618",
+            "subdomain": "entry-0",
+            "target": "127.0.0.1",
+            "ttl": 3600,
+            "zone": "hashicorp4noobs.fr"
+        },
+    ]
+
+    assert tfstate_content == tfstate_assert
