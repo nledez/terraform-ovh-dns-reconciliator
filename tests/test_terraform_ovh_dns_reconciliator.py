50911f7f33d608ee1ed95d4b8cf6887ba1a2d756
Nicolas Ledez <github.public@ledez.net>
2020-03-20 21:38:45 +0100

    Add test with load a tfstate with three records

--- tests/test_terraform_ovh_dns_reconciliator.py
+++ tests/test_terraform_ovh_dns_reconciliator.py
@@ -22,3 +22,38 @@ def test_load_tfstate_with_one_dns_record():
     ]
 
     assert tfstate_content == tfstate_assert
+
+
+def test_load_tfstate_with_three_dns_records():
+    ex2_filepath = os.path.join(DIR_PATH, 'terraform_ex2.tfstate')
+    tfstate_content = load_tfstate(ex2_filepath)
+
+
+    tfstate_assert = [
+        {
+            "fieldtype": "A",
+            "id": "5110317618",
+            "subdomain": "entry-0",
+            "target": "127.0.0.1",
+            "ttl": 3600,
+            "zone": "hashicorp4noobs.fr"
+        },
+        {
+            "fieldtype": "A",
+            "id": "5110317621",
+            "subdomain": "entry-1",
+            "target": "127.0.0.1",
+            "ttl": 3600,
+            "zone": "hashicorp4noobs.fr"
+        },
+        {
+            "fieldtype": "A",
+            "id": "5110317622",
+            "subdomain": "entry-2",
+            "target": "127.0.0.1",
+            "ttl": 3600,
+            "zone": "hashicorp4noobs.fr"
+        },
+    ]
+
+    assert tfstate_content == tfstate_assert
