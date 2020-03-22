52ca891e3a527973dab55dc0dc0cfb77b0279b92
Nicolas Ledez <github.public@ledez.net>
2020-03-22 16:25:08 +0100

    Add test with 2 DNS entries

--- tests/test_ovh_dns.py
+++ tests/test_ovh_dns.py
@@ -1,13 +1,21 @@
-import pytest
 import ovh
 
 from unittest import mock
 
 from terraform_ovh_dns_reconciliator import load_dns_entries
 
+
 def test_search_dns_records_with_one_entry():
     with mock.patch.object(ovh, 'Client') as mocked_client:
-        mocked_client.return_value.get.return_value = [5110321661]
+        mocked_client.return_value.get.return_value = [1234567890]
         dns_entries = load_dns_entries('hashicorp4noobs.fr', 'entry-1')
 
-        assert dns_entries == [5110321661]
+        assert dns_entries == [1234567890]
+
+
+def test_search_dns_records_with_two_entry():
+    with mock.patch.object(ovh, 'Client') as mocked_client:
+        mocked_client.return_value.get.return_value = [1234567890, 9876543210]
+        dns_entries = load_dns_entries('hashicorp4noobs.fr', 'entry-0')
+
+        assert dns_entries == [1234567890, 9876543210]
